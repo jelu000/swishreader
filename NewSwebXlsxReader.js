e345c9753eb7a96bb874eff2cda8c280acfe3d86
Jens L <jelu000@gmail.com>
2020-12-16 11:17:54 +0100

    Rättade till felet 2 sista rader av utgifter togs bort. Ändrade var till let för att scoopet ska bli rätt.

--- NewSwebXlsxReader.js
+++ NewSwebXlsxReader.js
@@ -2,81 +2,77 @@
 //https://docs.sheetjs.com/
   function handleFile(e) {
      //Get the files from Upload control
-        var files = e.target.files;
-        var i, f; //i=ieratior, f=files can be more then one
+        let files = e.target.files;
+        let i, f; //i=ieratior, f=files can be more then one
      //Loop through files
         for (i = 0, f = files[i]; i != files.length; ++i) {
-            var reader = new FileReader();
-            var name = f.name;
+            let reader = new FileReader();
+            //var name = f.name;
             reader.onload = function (e) {
-                var data = e.target.result;
+            let data = e.target.result;
 
-                var result;
-                //var workbook = XLSX.read(data, { type: 'binary' });
-                var workbook = XLSX.read(data, {type: 'array'});
-                var sheet = workbook.Sheets[workbook.SheetNames[0]]; // get the first worksheet
 
-                /* loop through every cell manually */
-                var range = XLSX.utils.decode_range(sheet['!ref']); // get the range
-                //range.s.r = 8; // <-- zero-indexed, so setting to 1 will skip row 0
-                //sheet['!ref'] = XLSX.utils.encode_range(range);
+            //var workbook = XLSX.read(data, { type: 'binary' });
+            let workbook = XLSX.read(data, {type: 'array'});
+            let sheet = workbook.Sheets[workbook.SheetNames[0]]; // get the first worksheet
 
-                let stoploop = false;
-                var data_array_table = [];
-//var trange = { s: { c: 0, r: 8 }, e: { c: 0, r: 4 }
+            /* loop through every cell manually */
+            let range = XLSX.utils.decode_range(sheet['!ref']); // get the range
+            //range.s.r = 8; // <-- zero-indexed, so setting to 1 will skip row 0
+            //sheet['!ref'] = XLSX.utils.encode_range(range);
 
-                //console.log("EndOfRange:" + range.e.r);
-                var i=0;
-                for(var R = range.s.r + 8; R <= range.e.r + 10000; ++R) {
 
-                  i++;
-                  //console.log("i=" +[i]);
+            let data_array_table = [];
+            //var trange = { s: { c: 0, r: 8 }, e: { c: 0, r: 4 }
 
-                  var tempRowArray = [];
+            //console.log("EndOfRange:" + range.e.r);
+            for(var R = range.s.r + 8; R <= range.e.r + 10000; ++R) {
 
-                  for(var C = range.s.c; C <= range.e.c; ++C) {
-                    /* find the cell object */
-                    //console.log('Row : ' + R);
-                    //console.log('Column : ' + C);
-                    var cellref = XLSX.utils.encode_cell({c:C, r:R}); // construct A1 reference for cell
-                    if(!sheet[cellref]) continue; // if cell doesn't exist, move on
-                    var cell = sheet[cellref];
-                    //console.log(cell.v);
+              //i++;
+              //console.log("i=" +[i]);
 
-                    tempRowArray.push(cell.v);//add all cells on a row
+              let tempRowArray = [];
 
-                  }//end of inner forloop - columns
+              for(var C = range.s.c; C <= range.e.c; ++C) {
+                /* find the cell object */
+                //console.log('Row : ' + R);
+                //console.log('Column : ' + C);
+                let cellref = XLSX.utils.encode_cell({c:C, r:R}); // construct A1 reference for cell
+                if(!sheet[cellref]) continue; // if cell doesn't exist, move on
 
-                  //Array strängen är tom, sluta mata rader och Bryter Loop!
-                  if (tempRowArray.toString() === "")
-                    break;
+                let cell = sheet[cellref];
+                //console.log(cell.v);
+                tempRowArray.push(cell.v);//add all cells on a row
 
-                  data_array_table.push(tempRowArray);//add a row to table
-                };//end of outer forloop - rows
+              }//end of inner forloop - columns
 
-                pickSwish(data_array_table);// add the data table to function to sort the sum on date
+              //console.log(tempRowArray);
+              //Array strängen är tom, sluta mata rader och Bryter Loop!
+              if (tempRowArray.toString() === "")
+                break;
 
-            };//end of function
-            reader.readAsArrayBuffer(f);
-        }//end of forloop - files
+              data_array_table.push(tempRowArray);//add a row to table
+          };//end of outer forloop - rows
+
+          pickSwish(data_array_table);// add the data table to function to sort the sum on date
+
+        };//end of function
+        reader.readAsArrayBuffer(f);
+      }//end of forloop - files
   }//end of function handleFile()
 //-----------------------------------------------------------------------------------------
 //Pic all the not Swish transaktions
 function pickOutcoms(twoD_Array){
 
 
-  var verTransArray =[];
+  let verTransArray =[];
   //var k=0;
-  for (i=2; i<twoD_Array.length; i++){
-    var trow_array = twoD_Array[i];
-    var swishstr = String(trow_array[4]);
+  for (i=0; i<twoD_Array.length; i++){
+    let trow_array = twoD_Array[i];
+    let swishstr = String(trow_array[4]);
     swishstr = swishstr.substring(0, 5);
     //console.log('swishstr ' + swishstr);
 
-    var strEight = String();
-
-
-    var strNine= String(trow_array[4]);
     //flytta refferens om de är bellop till rätt plats i radArrayen
     //console.log(trow_array[5]);
     if (trow_array[5] === ""){
@@ -88,8 +84,10 @@ function pickOutcoms(twoD_Array){
       //console.log(trow_array);
       //console.log('------------------');
 
-    if ('Swish' != swishstr) {
-      var trow =[trow_array[5], trow_array[1], trow_array[4], trow_array[6], trow_array[7]];
+    if ('Swish' !== swishstr) {
+      //console.log(trow_array);
+      //console.log('------------------');
+      let trow =[trow_array[5], trow_array[1], trow_array[4], trow_array[6], trow_array[7]];
       verTransArray.push(trow);
 
     }//end of if
@@ -106,14 +104,14 @@ function pickOutcoms(twoD_Array){
 //0-Radnr|1-Bokföringsdatum|2-Transaktionsdatun|3-Valutadatun|4-Transaktionstyp|5-Referens|6-Belopp|7-Saldo
 //new array: 1-Referens|2-Bokfdag|3-TransaktionsTyp|4-Belopp|5-Saldo
 function pickSwish(datatable) {
-  var newdatatable=[];
-  var j=0;
+  let newdatatable=[];
+  let j=0;
   for (i=0; i<datatable.length; i++){
 
-    var datarow = datatable[i];
-    var tswish = String(datarow[4]);
+    let datarow = datatable[i];
+    let tswish = String(datarow[4]);
     tswish = tswish.substring(0, 5);
-
+    //Om det inte är en swishtransaktion
     if (tswish=='Swish'){
       newdatatable[j]=[datarow[5], datarow[1], datarow[4], datarow[6], datarow[7]];
       j++;
@@ -138,9 +136,9 @@ function pickSwish(datatable) {
 //-----------------------------------------------------------------------------------------
 function newAddSumm(tdatatable, datatable){
 
-  var it=0;
-  var newtable = [];
-  var saldot = 0;
+  let it=0;
+  let newtable = [];
+  let saldot = 0;
 
   //lägger till en slut rad
   tdatatable.push(['0'],['0'],['0'],['0'],['0'],);
@@ -148,7 +146,7 @@ function newAddSumm(tdatatable, datatable){
   while (true) {
     datarow=tdatatable[it];
     datarowA=tdatatable[it+1];
-    var newrow=[];
+    let newrow=[];
 
     try {
       //console.log('--------------------------------------------');
@@ -197,7 +195,7 @@ function testPrint(t_datatable){
 //console.log(t_arra[0]);
 
   for (i=0; i<t_datatable.length; i++){
-    var t_row = t_datatable[i];
+    let t_row = t_datatable[i];
 
     //console.log(t_row[0] +':'+t_row[1]+':'+t_row[2]+':'+t_row[3] +':'+t_row[4]);
     //console.log(t_row);
@@ -209,39 +207,39 @@ function testPrint(t_datatable){
 
 function createHtmlTable(twoD_Array, tDataTable) {
   console.log("Jens Lundeqvist 2020-12-02");
-  var otherOutComes = pickOutcoms(tDataTable);
+  let otherOutComes = pickOutcoms(tDataTable);
+console.log(otherOutComes);
+  let maintabledata = "";
+  let omaintabledata = "";
 
-  var maintabledata = "";
-  var omaintabledata = "";
-
-  var html1 = "<!DOCTYPE html><html><head><title></title></head><body><div align='center'><br><strong>Swish transaktioner</strong>";
-  var html2 = "<br><br><table width='800px'>";
-  var tableheader_tag = "<tr><td><strong>Referens</strong></td><td><strong>Datum</strong></td><td><strong>TransaktionsTyp</strong></td><td><strong>Summa</strong></td><td><strong>Saldo</strong></td></tr>";
+  let html1 = "<!DOCTYPE html><html><head><title></title></head><body><div align='center'><br><strong>Swish transaktioner</strong>";
+  let html2 = "<br><br><table width='800px'>";
+  let tableheader_tag = "<tr><td><strong>Referens</strong></td><td><strong>Datum</strong></td><td><strong>TransaktionsTyp</strong></td><td><strong>Summa</strong></td><td><strong>Saldo</strong></td></tr>";
   for (i=0; i<twoD_Array.length; i++){
 
-    var trow_array = twoD_Array[i];
-    var trtd_tag = "<tr><td>" +trow_array[0]+ "</td><td>" +trow_array[1]+ "</td><td>" +trow_array[2]+ "</td><td>" +trow_array[3]+ "</td><td>" +trow_array[4]+ "</td></tr>";
+    let trow_array = twoD_Array[i];
+    let trtd_tag = "<tr><td>" +trow_array[0]+ "</td><td>" +trow_array[1]+ "</td><td>" +trow_array[2]+ "</td><td>" +trow_array[3]+ "</td><td>" +trow_array[4]+ "</td></tr>";
     maintabledata = maintabledata + trtd_tag;
 
   }//end of forloop
 
-  var html3 = "</table>";
-  var html4 = "</div></body></html>";
+  let html3 = "</table>";
+  let html4 = "</div></body></html>";
 
-  var andratrans = '<br><br><br><br><strong>Andra transaktioner</strong>';
+  let andratrans = '<br><br><br><br><strong>Andra transaktioner</strong>';
 
   for (i=0; i<otherOutComes.length; i++){
 
-    var orow_array = otherOutComes[i];
-    var ortd_tag = "<tr><td>" +orow_array[0]+ "</td><td>" +orow_array[1]+ "</td><td>" +orow_array[2]+ "</td><td>" +orow_array[3]+ "</td><td>" +orow_array[4]+ "</td></tr>";
+    let orow_array = otherOutComes[i];
+    let ortd_tag = "<tr><td>" +orow_array[0]+ "</td><td>" +orow_array[1]+ "</td><td>" +orow_array[2]+ "</td><td>" +orow_array[3]+ "</td><td>" +orow_array[4]+ "</td></tr>";
     omaintabledata = omaintabledata + ortd_tag;
 
   }//end of forloop
 
-  var html_body = html1 + html2 + tableheader_tag + maintabledata + html3 + andratrans + html2 + tableheader_tag+ omaintabledata + html3 + html4;
+  let html_body = html1 + html2 + tableheader_tag + maintabledata + html3 + andratrans + html2 + tableheader_tag+ omaintabledata + html3 + html4;
 //console.log(html_body);
   demo();
-  var t_opened = window.open("", "_self");
+  let t_opened = window.open("", "_self");
   t_opened.document.write(html_body);
 
 
